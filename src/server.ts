87be2429b590ebbec7813f8f3b1d57bd1caac3b2
Sam Holtby <web@passiondigital.co.uk>
2023-02-17 14:51:25 +0100

    removed module-alias and prevent multiple submissions of /sync

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,15 @@
-import express, { NextFunction, Request, Response } from 'express'
-import 'module-alias/register'
+import express, { Request, Response } from 'express'
 import '@config/setup'
 import CONFIG from '@root/config'
 import syncSites from '@lib/actions'
+import { logger } from '@lib/logger'
 import { isAppEngineCron } from '@lib/middleware'
 
 const app = express()
 const PORT = CONFIG.PORT || 8080
 
 app.listen(PORT, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Server listening on port ${PORT}...`)
+    logger.info(`Server listening on port ${PORT}...`)
 })
 
 // ensure that request comes from app engine cron
@@ -18,7 +17,25 @@ if (CONFIG.NODE_ENV === 'production') {
     app.use(isAppEngineCron)
 }
 
-app.get('/synchronize', (req: Request, res: Response, next: NextFunction) => {
-    syncSites()
-    res.send('syncing sites!')
+app.get('/synchronize', async (req: Request, res: Response) => {
+    try {
+        const isProcessing = req.app.get('isProcessing')
+        if (isProcessing) {
+            logger.error('Syncing locked, cannot process request.')
+            return res.status(400).send('Processing in progress, please wait.')
+        }
+
+        req.app.set('isProcessing', true)
+        logger.info('Syncing locked')
+
+        syncSites().then(() => {
+            req.app.set('isProcessing', false)
+            logger.info('Syncing unlocked')
+        })
+
+        res.send('syncing sites!')
+    } catch (error) {
+        logger.error('An error occurred while processing:', error)
+        res.status(500).send('An error occurred while processing.')
+    }
 })
--- src/sync-sites.ts
+++ src/sync-sites.ts
@@ -1,4 +1,3 @@
-import 'module-alias/register'
 import '@root/config/setup'
 import syncSites from '@lib/actions'
 
